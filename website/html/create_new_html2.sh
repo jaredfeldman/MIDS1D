2dc2600019a21428ccc06ba64ca4519a35a95d37
jaredfeldman <jared.alex.feldman@gmail.com>
2022-07-20 12:13:13 +0000

    new working scripts added and modified

--- website/html/create_new_html2.sh
+++ website/html/create_new_html2.sh
@@ -6,8 +6,37 @@ ls images > imagelist.txt
 # use new line at delimiter
 IFS=$'\n' 
 
-# loop thru the lines in the new imagelist.txt file to return the name of each image
+# loop thru the lines in the new imagelist.txt file to return the name of each image and then replace the name in html
 for line in $(cat ./imagelist.txt)
 do
-	echo "<td><img src="\"images/"$line\" alt=\"\" border=3 height=100 width=300></th>" >> testindex.html #preface each image file name with directory name images\
+	sed "s/<td>\(.*\)<\/th>/<td><img src='\'images\/'$line\' alt=\'\' border=3 height=100 width=300><\/th>/" index.html
 done
+
+
+# now loop thru the index.html file and find each and replace with filename
+#replaceline='<td>\(.*\)</th>'
+#for line in $(cat ./index.html)
+#do
+#	if [ $line = $replaceline ]
+#	then
+#		sed 's/$replaceline/hello/'
+#		echo "hi"
+#	fi
+#done
+
+# indicate the matching line and use leading spaces
+match="    </tr>"
+replace="    hello"
+
+# for every line in the index.html file, find line that matches $match and replace with sed
+for line in $(cat ./index.html)
+do
+	if [ $line = $match ]
+	then
+		sed "s|$line|$replace|" index.html
+	fi
+done
+
+
+# test sed to replace only the URL
+# sed  s/<td>\(.*\)<\/th>/hello/' index.html
