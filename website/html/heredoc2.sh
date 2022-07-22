e1397a60992aeb691b6531939d6c8e97a47c8bf4
jaredfeldman <jared.alex.feldman@gmail.com>
2022-07-22 20:50:25 +0000

    generated site with 3 columns of images, almost there!

--- website/html/heredoc2.sh
+++ website/html/heredoc2.sh
@@ -19,9 +19,16 @@ fi
 # and prefance each image file name with images\ directory
 for line in $(cat ./imagelist.txt)
 do
-	echo "<td><img src="\"images/"$line\ alt=\"\" border=3 height=100 width=300</th>" >> replacement_images.txt
+	echo "        <td><img src="\"images/"$line\" alt=\"\" border=3 height=100 width=300</th>" >> replacement_images.txt
 done
 
+
+# remove any existing file named temp_index.html
+temp_html_file=temp_index.html
+if [[ -f "$html_file" ]]
+then
+	rm temp_index.html
+fi
 
 # remove any existing file named index.html
 html_file=index.html
@@ -30,8 +37,9 @@ then
 	rm index.html
 fi
 
+
 # create the template index.html
-cat << EOF > index.html
+cat << EOF > temp_index.html
 <!doctype html>
 <html>
   <head>
@@ -64,46 +72,35 @@ iterations=$(($num_images / $columns))
 # find INSERT_HERE in newly created index.html and add lines to be replaced by image filenames
 while [ $iterations -gt 0 ]
 do
-	sed -i 's|INSERT_HERE|    <tr>\nreplace\nreplace\nreplace\n    </tr>\nINSERT_HERE|' index.html
+	sed -i 's|INSERT_HERE|    <tr>\nreplace\nreplace\nreplace\n    </tr>\nINSERT_HERE|' temp_index.html
 	((iterations=iterations-1))
 done
 
 # remove the remaining INSERT_HERE from index.html
-sed -i 's|INSERT_HERE||' index.html
+sed -i 's|INSERT_HERE||' temp_index.html
 
 # loop thru each line in index.html and replace "replace" with the top
 # line in replacement_images.txt, followed by deleting the top line
 set -x
 line_num=1
-firstline=$(head -n 1 replacement_images.txt)
-for line in $(cat index.html)
+#firstline=$(head -n 1 replacement_images.txt)
+for line in $(cat temp_index.html)
 do
+declare firstline=$(head -n 1 replacement_images.txt)
 	if [[ $line == "replace" ]]
 	then
+		echo $line >> index.html; sed -i "s,replace,$firstline," index.html; sed -i '1d' replacement_images.txt
+	else
+		echo $line >> index.html
+	fi
+#	if [[ $line == "replace" ]]
+#	then
 #		echo $firstline; sed -i '1d' replacement_images.txt
-		sed -i "s|table|,|replace|${firstline}|" index.html; sed -i '1d' replacement_images.txt
+#		sed -i "s|table|,|replace|${firstline}|" index.html; sed -i '1d' replacement_images.txt
 #		sed -i '/table/,/replace/$(sed -n ${line_num}p < replacement_images.txt)/' index.html; ((line_num+=1)); echo $line_num
 #		 sed -i "s|${line}|        $(sed -n ${line_num}p < replacement_images.txt)|" index.html; ((line_num+=1)); echo $line_num
-	else
-		continue
-	fi
+#	else
+#		continue
+#	fi
 #	echo "<td><img src="\"images/"$line\ alt=\"\" border=3 height=100 width=300</th>" >> replacement_images.txt
 done
-
-
-
-
-
-
-
-
- ## in case needed later - iterates and creates new index.html file
-
-#echo -e "hello\nhello\nline3\nreplace\nreplace\nreplace" >> index.html
-
-
-#while [ $iterations -gt 0 ]
-#do
-#	echo -e "    <tr>\nreplace\nreplace\nreplace\n    </tr>" >> index.html
-#	((iterations=iterations-1))
-#done
